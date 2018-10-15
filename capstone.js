04ec6033f04e15aa351cb533398bbcf18140bae9
Satori <comfycallum@gmail.com>
2018-10-15 00:30:40 +0000

    remove js files

--- capstone.js
+++
@@ -1,6 +0,0 @@
-const fsn = require('fs');
-
-let filedata = fsn.readFileSync('./lib/capstone.min.js','utf8');
-eval(filedata);
-
-module.exports = capstone;
--- keystone.js
+++
@@ -1,6 +0,0 @@
-const fsn = require('fs');
-
-let filedata = fsn.readFileSync('./lib/keystone.min.js','utf8');
-eval(filedata);
-
-module.exports = ks;
